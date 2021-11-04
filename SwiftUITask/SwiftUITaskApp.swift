5352f0286f9dd59d279b980d20bc0f53091cb28d
brainox <macmartins081@gmail.com>
2021-11-04 16:51:47 +0100

    Initial Commit

---
+++ SwiftUITask/SwiftUITaskApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUITaskApp.swift
+//  SwiftUITask
+//
+//  Created by Decagon on 04/11/2021.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUITaskApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
